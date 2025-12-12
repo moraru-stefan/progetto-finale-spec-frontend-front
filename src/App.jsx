35c81b75d27bd05b79cebfd784363ddcb1d4ecec
Steopa2001 <moraru495@gmail.com>
2025-12-12 10:50:50 +0100

    [feat] implement smartphone list fetching and rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { useState, useEffect } from "react";
 
 function App() {
+  // Stato che contiene la lista degli smartphone
+  const [smatphones, setSmartphones] = useState([]);
 
+  useEffect(() => {
+    // Recupero dei dati dal backend all'avvio dell'app
+    fetch("http://localhost:3001/smartphones")
+      // Converto la risposta in JSON
+      .then((res) => res.json())
+      // Salvo i dati nello stato
+      .then((data) => setSmartphones(data))
+      // Gestione errori
+      .catch((err) => console.error("Errore nel fetch:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista Smartphone</h1>
+      <ul>
+        {/* Ciclo sugli smartphone e li mostro nella lista */}
+        {smatphones.map((smatphone) => {
+          return (
+            <li key={smatphone.id}>
+              {smatphone.title} - {smatphone.category}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
