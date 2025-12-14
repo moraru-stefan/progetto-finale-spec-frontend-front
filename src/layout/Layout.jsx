eb873a109921fc8ca5ebd563d4a20a066936d606
Steopa2001 <moraru495@gmail.com>
2025-12-14 20:30:47 +0100

    [feat] add scroll-to-top button with styling

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,12 +5,22 @@ const Layout = () => {
     <div className="container py-3">
       <nav className="mb-3 d-flex justify-content-center align-items-center">
         <div className="app-navbar">
-          <NavLink to="/" className="nav-link">Home</NavLink>
-          <NavLink to="/compare" className="nav-link">Confronta</NavLink>
-          <NavLink to="/favorites" className="nav-link">Preferiti</NavLink>
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/compare" className="nav-link">
+            Confronta
+          </NavLink>
+          <NavLink to="/favorites" className="nav-link">
+            Preferiti
+          </NavLink>
         </div>
       </nav>
       <Outlet />
+      <i
+        className="fa-regular fa-circle-up scroll-top-btn"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      ></i>
     </div>
   );
 };
