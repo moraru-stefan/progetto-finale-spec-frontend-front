87d2cc5e995d7218e5bf6a9a0c003884cb4b8603
Steopa2001 <moraru495@gmail.com>
2025-12-17 14:58:58 +0100

    [feat] move function that clears all favorites at the top

--- src/App.jsx
+++ src/App.jsx
@@ -39,11 +39,16 @@ function App() {
     const saved = localStorage.getItem("compareIds");
     return saved ? JSON.parse(saved) : [];
   });
-
+  
   useEffect(() => {
     localStorage.setItem("compareIds", JSON.stringify(compareIds));
   }, [compareIds]);
-
+  
+  // Funzione per svuotare tutti i preferiti
+  function clearFavorites() {
+    setFavorites([]);
+  }
+  
   // Funzione per aggiungere o rimuovere uno smartphone dal confronto
   function toggleCompare(id) {
     setCompareIds((prev) => {
@@ -71,10 +76,6 @@ function App() {
       .catch((err) => console.error("Errore nel fetch:", err));
   }, []);
 
-  // Funzione per svuotare tutti i preferiti
-  function clearFavorites() {
-    setFavorites([]);
-  }
 
   return (
     <BrowserRouter>
