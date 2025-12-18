38f8c8be3b5450c55a423627f3041e27081d9e1c
Steopa2001 <moraru495@gmail.com>
2025-12-18 11:36:11 +0100

    [fix] remove clearCompare prop from ComparePage.jsx

--- src/pages/ComparePage.jsx
+++ src/pages/ComparePage.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 const API_URL = import.meta.env.VITE_API_URL;
 
 
-const ComparePage = ({ compareIds, clearCompare }) => {
+const ComparePage = ({ compareIds }) => {
   const [phones, setPhones] = useState([]);
 
 useEffect(() => {
